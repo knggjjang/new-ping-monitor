49a01908c50232ff31681696ad65bebc6c8211b5
윤제용 <jyyun@yunjeyong-ui-MacBookAir.local>
2026-04-22 11:43:53 +0900

    release: v0.4.2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import TitleBar from "@/components/TitleBar";
 
 export const metadata: Metadata = {
   title: "New Ping Monitor",
@@ -15,8 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className="bg-transparent antialiased">
-        <TitleBar />
-        <main className="pt-8 h-screen w-screen overflow-hidden">
+        <main className="h-screen w-screen overflow-hidden">
           {children}
         </main>
       </body>
